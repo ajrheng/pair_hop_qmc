c8557239719a73d21f1b9379bd7d620efeb8b9d5
Alvin Heng <alvin_heng95@hotmail.com>
2020-04-15 22:55:59 +0800

    debug files

---
+++ python/debugger.py
@@ -0,0 +1,24 @@
+from dimer_debug_class import *
+
+if __name__ == "__main__":
+
+    f = open('log.txt','w') 
+    f.close()
+    m = open('error.txt','w')
+    m.close()
+
+    test = mc_sse_dimer(1.2,0,10,2,equil_steps = 1, mc_steps = 1000, num_runs = 1)
+
+    test.init_state()
+    test.lattice()
+    test.init_matrix_ele()
+    test.pvect0()
+    test.vxweight()
+    test.initvrtx()
+
+    for i in range(test.mc_steps):
+        print('\n STARTING MC STEP ', i)
+        print(test.state)
+        if (i+1)%100 == 0:
+            test.adjust_trun_cutoff()
+        test.one_mc_step(i,0)
